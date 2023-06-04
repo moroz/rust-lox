fb1b1b0c87c13279d79d573875ff95a0f002cf3f
Karol Moroz <karol@moroz.dev>
2023-06-04 03:21:36 -0600

    Stub Lox struct

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
 use std::{env, fs};
 
+struct Lox {
+    had_error: bool,
+}
+
+impl Lox {
+    fn new() -> Self {
+        Self { had_error: false }
+    }
+
+    fn report(mut self, line: isize, location: String, message: String) {
+        println!("[line {line}] Error {location}: {message}");
+        self.had_error = true;
+    }
+
+    fn run(mut self, source: String) {
+        let mut scanner = Scanner::new(source);
+        let tokens = scanner.scan_tokens();
+
+        for token in tokens {
+            println!("{:?}", token);
+        }
+    }
+
+    fn run_file(self, filename: String) {
+        let contents = fs::read_to_string(filename).unwrap();
+        self.run(contents);
+    }
+}
+
 struct Scanner {
     source: String,
     has_error: bool,
@@ -21,27 +50,14 @@ impl Scanner {
     }
 }
 
-fn run(source: String) {
-    let mut scanner = Scanner::new(source);
-    let tokens = scanner.scan_tokens();
-
-    for token in tokens {
-        println!("{:?}", token);
-    }
-}
-
-fn run_file(filename: String) {
-    let contents = fs::read_to_string(filename).unwrap();
-    run(contents);
-}
-
 fn main() {
     if env::args().len() != 2 {
         println!("Usage: lox [script]");
         std::process::exit(64);
     } else if env::args().len() == 2 {
         let args: Vec<_> = env::args().collect();
-        run_file(args[1].clone());
+        let mut lox = Lox::new();
+        lox.run_file(args[1].clone());
     }
     //     run_
     // }
