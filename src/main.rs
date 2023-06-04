6128bf23302d67b500b2353e46c79b0ba07638e9
Karol Moroz <karol@moroz.dev>
2023-06-04 03:15:27 -0600

    Implement main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use std::{env, fs};
+
+struct Scanner {
+    source: String,
+    has_error: bool,
+}
+
+#[derive(Debug)]
+struct Token {}
+
+impl Scanner {
+    fn new(source: String) -> Self {
+        Self {
+            source,
+            has_error: false,
+        }
+    }
+
+    fn scan_tokens(self) -> Vec<Token> {
+        unimplemented!()
+    }
+}
+
+fn run(source: String) {
+    let mut scanner = Scanner::new(source);
+    let tokens = scanner.scan_tokens();
+
+    for token in tokens {
+        println!("{:?}", token);
+    }
+}
+
+fn run_file(filename: String) {
+    let contents = fs::read_to_string(filename).unwrap();
+    run(contents);
+}
+
 fn main() {
-    println!("Hello, world!");
+    if env::args().len() != 2 {
+        println!("Usage: lox [script]");
+        std::process::exit(64);
+    } else if env::args().len() == 2 {
+        let args: Vec<_> = env::args().collect();
+        run_file(args[1].clone());
+    }
+    //     run_
+    // }
 }
