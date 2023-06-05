db36a25f5103896048bf15ffb4e524c6d190762a
Karol Moroz <karol@moroz.dev>
2023-06-05 04:42:00 -0600

    Add run_prompt option

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use parser::Parser;
 
 use crate::scanner::Scanner;
-use std::{env, fs};
+use std::{env, fs, io::Write};
 
 mod expr;
 mod parser;
@@ -15,7 +15,7 @@ impl Lox {
         Self
     }
 
-    fn run(self, source: String) {
+    fn run(&self, source: String) {
         let mut scanner = Scanner::new(source);
         let tokens = scanner.scan_tokens();
 
@@ -37,15 +37,39 @@ impl Lox {
         let contents = fs::read_to_string(filename).unwrap();
         self.run(contents);
     }
+
+    fn run_prompt(&self) {
+        let mut buffer = String::new();
+
+        loop {
+            print!("> ");
+            std::io::stdout().flush().unwrap();
+            match std::io::stdin().read_line(&mut buffer) {
+                Ok(0) => {
+                    break;
+                }
+                Ok(_) => {
+                    self.run(buffer.clone());
+                    buffer.clear();
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+    }
 }
 
 fn main() {
-    if env::args().len() != 2 {
+    if env::args().len() > 2 {
         println!("Usage: lox [script]");
         std::process::exit(64);
     } else if env::args().len() == 2 {
         let args: Vec<_> = env::args().collect();
         let lox = Lox::new();
         lox.run_file(args[1].clone());
+    } else {
+        let lox = Lox::new();
+        lox.run_prompt();
     }
 }
