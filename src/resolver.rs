13634e89a71e80fe19c8b9a84d93da3bacd498b0
Karol Moroz <k.j.moroz@gmail.com>
2023-06-11 02:44:21 +0200

    Stub resolver struct

--- src/resolver.rs
+++ src/resolver.rs
@@ -1,9 +1,43 @@
 use std::collections::HashMap;
 
-use crate::token::Token;
+use crate::{errors::LoxError, stmt::Stmt, token::Token};
 
-pub struct Resolver;
+pub struct Resolver {
+    scopes: Vec<HashMap<String, bool>>,
+}
 
 pub type ResolutionMap = HashMap<Token, usize>;
 
-impl Resolver {}
+#[derive(Clone, Debug, PartialEq, Eq)]
+pub enum ResolutionError {}
+
+pub type ResolutionResult<T> = Result<T, ResolutionError>;
+
+impl Resolver {
+    #[must_use]
+    fn new() -> Self {
+        Self { scopes: Vec::new() }
+    }
+
+    pub fn visit_statement(&mut self, stmt: Stmt) -> ResolutionResult<()> {
+        match stmt {
+            Stmt::Block(statements) => {
+                self.begin_scope();
+                self.resolve(statements);
+                self.end_scope();
+                Ok(())
+            }
+            _ => {
+                unimplemented!()
+            }
+        }
+    }
+
+    fn begin_scope(&mut self) {
+        self.scopes.push(HashMap::new());
+    }
+
+    fn end_scope(&mut self) {
+        self.scopes.pop();
+    }
+}
